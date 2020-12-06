c2f1bc3f545c7e8e50f9f29c8998f1f5072b2a3d
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-06 10:48:00 +0200

    Add department class

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.util.Objects;
+
+public class Department {
+    private int id;
+    private String name;
+    private String description;
+
+
+    public Department(String name,String description) {
+        this.description = description;
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+}
